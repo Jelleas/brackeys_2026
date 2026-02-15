bb27a0581e2ea08b96d2bdfd8949af25f863eeb0
jelleas <jelle.van.assema@gmail.com>
2026-02-15 18:30:41 +0100

    typing

---
+++ input_label.gd
@@ -0,0 +1,35 @@
+extends Label
+
+var typed = []
+
+func _init() -> void:
+	pass
+	
+func _process(delta: float) -> void:
+	pass
+	
+func _unhandled_input(event: InputEvent) -> void:
+	if not event is InputEventKey:
+		return
+	
+	if not event.is_pressed():
+		return
+	
+	var keycode = event.keycode
+	
+	if (keycode < KEY_A || keycode > KEY_Z) && keycode != KEY_SPACE:
+		return
+		
+	var character = String.chr(event.keycode)
+	
+	if not event.shift_pressed:
+		character = character.to_lower()
+	
+	typed.append(character)
+	
+	update()
+	
+
+func update() -> void:
+	self.text = "".join(typed)
+	
