816da191c06f7603e77ac1e4a9b2f57400cef045
Wietze <wietze@wietzes-macbook-pro.home>
2026-02-15 21:12:25 +0100

    rename to ball and add wizard basics

--- spells/ball.gd
+++ spells/ball.gd
@@ -11,6 +11,22 @@ func _ready():
 	collision_mask = 1
 	add_to_group("projectile")
 
+func set_colors(center_color: Color, mid_color: Color, edge_color: Color):
+	$Sprite2D.modulate = center_color
+
+	mat = $GPUParticles2D.process_material as ParticleProcessMaterial
+	
+	var gradient = Gradient.new()
+	gradient.colors = PackedColorArray([
+		center_color,
+		mid_color,
+		Color(edge_color.r, edge_color.g, edge_color.b, 0.0)
+	])
+	gradient.offsets = PackedFloat32Array([0.0, 0.5, 1.0])
+
+	var grad_tex = GradientTexture1D.new()
+	grad_tex.gradient = gradient
+	mat.color_ramp = grad_tex
 
 func setup_shape(range: float):
 	$CollisionShape2D.shape.radius = range
