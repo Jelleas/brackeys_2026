74d3887571fe6ac7f7fd390dcdd136ef3a940c2b
Wietze <wietze@wietzes-macbook-pro.home>
2026-02-15 21:32:06 +0100

    add a wizard base and let it cast balls

--- wizard/wizard.gd
+++ wizard/wizard.gd
@@ -1,1 +1,18 @@
 extends Node2D
+
+var ball_scene = preload("res://spells/ball.tscn")
+
+var timer: float = 0.0
+func _process(delta: float) -> void:
+	timer += delta
+	if timer >= 1.0:
+		timer = 0.0
+		cast_spell(50.0, Color("#AAEEFF"), Color("#3388FF"), Color("#1122AA"))
+		#cast_spell(50.0, Color("#FFFFAA"), Color("#FF6600"), Color("#CC2200"))
+
+func cast_spell(spell_range: float, center_color: Color, mid_color: Color, edge_color: Color):
+	var ball = ball_scene.instantiate()
+	ball.global_position = global_position
+	get_tree().current_scene.add_child(ball)
+	ball.setup_shape(spell_range)
+	ball.set_colors(center_color, mid_color, edge_color)
