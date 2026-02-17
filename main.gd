31e817fbaa80b0abd6b16084809372d1d3aa4de0
Dasyel <dasyelwillems@gmail.com>
2026-02-17 22:33:58 +0100

    fix pausing

--- main.gd
+++ main.gd
@@ -7,6 +7,7 @@ class_name Main extends Node2D
 func _ready() -> void:
 	Bus.wizard_killed.connect(_on_wizard_killed)
 	Bus.new_game.connect(_on_new_game)
+	Bus.tutorial_over.connect(_on_tutorial_over)
 	await get_tree().process_frame
 	await get_tree().process_frame
 	get_tree().paused = true
@@ -18,3 +19,6 @@ func _on_wizard_killed():
 func _on_new_game():
 	game_screen = game_scene.instantiate()
 	add_child(game_screen)
+	
+func _on_tutorial_over():
+	get_tree().paused = false
